3c4c496ff6185e220cc62e626eda54663ca7c71f
ryanfalco13@gmail.com <ryanfalco13@gmail.com>
2023-06-09 00:55:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,22 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(titleCase);
+};
+
+const titleCase = (str) => {
+  return str
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+};
+
+const titleCasedTutorials = titleCased();
+console.log(titleCasedTutorials);
+
+
+
+
+
+
+
